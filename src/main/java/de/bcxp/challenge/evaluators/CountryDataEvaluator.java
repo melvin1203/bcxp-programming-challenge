aa8df0eef1eb5e3c4698ea61880b732ab51bc479
melvin1203 <melvin.grzalik@gmail.com>
2026-09-15 19:17:14 +0200

    feat: implement country evaluator for maximum population density

---
+++ src/main/java/de/bcxp/challenge/evaluators/CountryDataEvaluator.java
@@ -0,0 +1,25 @@
+package de.bcxp.challenge.evaluators;
+
+
+import de.bcxp.challenge.models.CountryData;
+
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Evaluates countries data and returns the name of the country with the highest population density.
+ */
+public class CountryDataEvaluator implements DataEvaluator<CountryData, String> {
+
+    @Override
+    public String evaluate(List<CountryData> data) {
+        if (data == null || data.isEmpty()) {
+            throw new IllegalArgumentException("data is null or empty");
+        }
+
+        return data.stream()
+                .max(Comparator.comparingDouble(CountryData::getPopulationDensity))
+                .map(CountryData::name)
+                .orElseThrow(() -> new IllegalArgumentException("No data available"));
+    }
+}
