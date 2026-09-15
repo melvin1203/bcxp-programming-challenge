30d24c5c13b1484012d3db0c2add052d88efab3b
melvin1203 <melvin.grzalik@gmail.com>
2026-09-15 13:16:26 +0200

    feat: implement weather evaluator for minimum temperature spread

---
+++ src/main/java/de/bcxp/challenge/evaluators/WeatherDataEvaluator.java
@@ -0,0 +1,23 @@
+package de.bcxp.challenge.evaluators;
+
+import de.bcxp.challenge.models.WeatherData;
+
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Evaluates weather data to find the day with the smallest temperature spread.
+ */
+public class WeatherDataEvaluator implements DataEvaluator<WeatherData, Integer> {
+    @Override
+    public Integer evaluate(List<WeatherData> data) {
+
+        if (data == null || data.isEmpty()) {
+            throw new IllegalArgumentException("data is null or empty");
+        }
+        return data.stream()
+                .min(Comparator.comparingDouble(WeatherData::getTempSpread))
+                .map(WeatherData::day)
+                .orElseThrow(() -> new IllegalArgumentException("No data available"));
+    }
+}
